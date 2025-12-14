779b22f83ea839bfb2fa288ca69886a248ee6b28
Moksh-10 <agmoksh10@gmail.com>
2025-12-15 00:01:06 +0530

    tr

--- binary_trees.py
+++ binary_trees.py
@@ -14,6 +14,7 @@ class bst:
     def __init__(self):
         self.root = None
 
+    # O(n) - worst  O(log n) - avg  O(h) - always
     def __contains__(self, key):
         cn = self.root
         while cn is not None:
@@ -25,9 +26,11 @@ class bst:
                 return True
         return False
 
+    # O(n)
     def __iter__(self):
         yield from self._in_order(self.root)
 
+    # O(n)
     def __repr__(self):
         return str(list(self._in_order(self.root)))
 
@@ -80,6 +83,7 @@ class bst:
             raise KeyError('does not exist')
         self._delete(node)
 
+    # O(n)
     def traverse(self, order):
         if order == 'inorder':
             yield from self._in_order(self.root)
@@ -121,7 +125,7 @@ class bst:
             suc = self._successor(node)
             node.key = suc.key
             node.value = suc.value
-            self.delete(suc)
+            self._delete(suc)
 
     def _successor(self, node: Node):
         if node is None:
@@ -151,16 +155,39 @@ class bst:
             yield (node.key, node.value)
             yield from self._in_order(node.right)
 
-    def _pre_order(self):
+    def _pre_order(self, node: Node):
         if node is not None:
             yield (node.key, node.value)
             yield from self._pre_order(node.left)
             yield from self._pre_order(node.right)
 
-    def _post_order(self):
+    def _post_order(self, node: Node):
         if node is not None:
             yield from self._post_order(node.left)
             yield from self._post_order(node.right)
             yield (node.key, node.value)
 
+
+if __name__ == "__main__":
+    tr = bst()
+    tr.insert(10, 'x')
+    tr.insert(5, 'x')
+    tr.insert(22, 'x')
+    tr.insert(2, 'x')
+    tr.insert(9, 'x')
+    tr.insert(12, 'x')
+    tr.insert(30, 'x')
+    tr.insert(11, 'x')
+    tr.insert(15, 'x')
+    tr.insert(30, 'x')
+    tr.insert(23, 'x')
+    tr.insert(35, 'x')
+    tr.delete(9)
+    tr.delete(10)
+    tr.insert(1, 'x')
+    tr.delete(2)
+    for i in tr.traverse('inorder'):
+        print(i)
+    print(tr)
+    print(tr.search(30))
 
